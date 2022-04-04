bec8ddab5f8ee8f84568469939e0672c9a8387a8
elias <eliasfonseca94@gmail.com>
2022-04-04 14:25:37 -0400

    function

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+
+    $('a').click(function () {
+        var gato = this.hash
+
+        $('html, body'),animate(
+            {
+
+                scrolltop: $(gato).offset().top
+            },
+            3000
+            
+        )
+    })
+}
