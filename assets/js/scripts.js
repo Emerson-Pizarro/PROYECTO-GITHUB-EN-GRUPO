e737e50fdfd471c0e34baf4a972904ae987336fa
Emerson <emerson_pizarro@outlook.com>
2022-04-04 14:07:21 -0400

    puesta de links en los iconos del footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
             scrollTop: $(hash).offset().top
-            }, 1400, function(){
+            }, 9000, function(){
     
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
